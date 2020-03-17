3c8d3bddeb8ce5e74c7d477535ca3b70549c8e65
Maurício Neto <malbuquerqueneto@gmail.com>
2020-03-16 21:13:45 -0300

    logger in dragonsclient

--- src/main/java/com/bigbank/dragons/client/DragonsClient.java
+++ src/main/java/com/bigbank/dragons/client/DragonsClient.java
@@ -1,6 +1,8 @@
 package com.bigbank.dragons.client;
 
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -12,35 +14,55 @@ import com.bigbank.dragons.model.Reputation;
 @Component
 public class DragonsClient {
 
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(DragonsClient.class);
+
+
   @Autowired
   private WebClient webClient;
 
   public Game startGame() {
-    return webClient.post().uri("/game/start").retrieve().bodyToMono(Game.class).block();
+    Game game = webClient.post().uri("/game/start").retrieve().bodyToMono(Game.class).block();
+    LOGGER.info("Game started: {}", game);
+    return game;
   }
 
   public Reputation investigateReputation(String gameId) {
-    return webClient.post().uri("/{gameId}/investigate/reputation", gameId).retrieve()
-        .bodyToMono(Reputation.class).block();
+    Reputation reputation = webClient.post().uri("/{gameId}/investigate/reputation", gameId)
+        .retrieve().bodyToMono(Reputation.class).block();
+    LOGGER.info("Reputation: {}", reputation);
+    return reputation;
   }
 
   public List<Ad> getMessages(String gameId) {
-    return webClient.get().uri("/{gameId}/messages", gameId).retrieve().bodyToFlux(Ad.class)
+    List<Ad> ads = webClient.get().uri("/{gameId}/messages", gameId).retrieve().bodyToFlux(Ad.class)
         .collectList().block();
+    for (Ad ad : ads) {
+      LOGGER.info("Ad: {}", ad);
+    }
+    return ads;
   }
 
   public Game solveMessage(String gameId, String adId) {
-    return webClient.post().uri("/{gameId}/solve/{adId}", gameId, adId).retrieve()
+    Game game = webClient.post().uri("/{gameId}/solve/{adId}", gameId, adId).retrieve()
         .bodyToMono(Game.class).block();
+    LOGGER.info("Solve message result: {}", game);
+    return game;
   }
 
   public List<Item> getShop(String gameId) {
-    return webClient.get().uri("/{gameId}/shop", gameId).retrieve().bodyToFlux(Item.class)
-        .collectList().block();
+    List<Item> items = webClient.get().uri("/{gameId}/shop", gameId).retrieve()
+        .bodyToFlux(Item.class).collectList().block();
+    for (Item item : items) {
+      LOGGER.info("Item: {}", item);
+    }
+    return items;
   }
 
   public Game buyItem(String gameId, String itemId) {
-    return webClient.post().uri("/{gameId}/shop/buy/{itemId}", gameId, itemId).retrieve()
+    Game game = webClient.post().uri("/{gameId}/shop/buy/{itemId}", gameId, itemId).retrieve()
         .bodyToMono(Game.class).block();
+    LOGGER.info("Shopping result: {}", game);
+    return game;
   }
 }
